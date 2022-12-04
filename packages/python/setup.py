7217122f33ef5f3ff9b5329e2a0ee83e4ef79d1d
AmirW <eng.amir.bu@gmail.com>
2022-12-04 16:08:39 +0330

    generate grpc protos for python and setup pip config

---
+++ packages/python/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+__version__ = "dev"
+
+setup(
+    name="sayif_protos",
+    version=__version__,
+    url="https://github.com/SayIfOrg/say_protos",
+    author="Amir Khalife",
+    author_email="eng.amir.bu@gmail.com",
+    py_modules=find_packages(),
+    install_requires=[
+        "grpcio==1.50",
+    ],
+)
