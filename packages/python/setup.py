5bda5c0c503769c40817dd4daade497d96c0283e
AmirW <eng.amir.bu@gmail.com>
2023-04-27 18:10:14 +0330

    fix error installing py package

--- packages/python/setup.py
+++ packages/python/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "dev"
+__version__ = "0.1.0"
 
 setup(
     name="sayif_protos",
@@ -10,7 +10,7 @@ setup(
     author_email="eng.amir.bu@gmail.com",
     py_modules=find_packages(),
     install_requires=[
-        "grpcio>=1.50,<1.51"
+        "grpcio>=1.50,<1.51",
         "grpcio-tools>=1.50,<1.51"
     ],
 )
